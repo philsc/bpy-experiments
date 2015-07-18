22165e760022a23c10ac65f892010195c785cb38
Philipp Schrader <philipp.schrader@gmail.com>
2015-07-17 22:35:23 -0700

    The cubes will now rotate around their own axis.
    Render as follows:
    blender -b empty.blend -P foo.py -x 1 -o //file -F MPEG -s 000 -e 100 -a

--- foo.py
+++ foo.py
@@ -4,6 +4,8 @@ import math
 NUM_CUBES = 20
 DISTANCE = 8
 
+cubes = []
+
 for i in range(0, NUM_CUBES):
     angle = i * 2 * math.pi / NUM_CUBES
     x = math.sin(angle) * DISTANCE
@@ -12,4 +14,26 @@ for i in range(0, NUM_CUBES):
     bpy.ops.mesh.primitive_cube_add(radius=1, location=(x, y, 0), \
             rotation=(0, 0, 0))
 
+    cubes.append(bpy.context.active_object)
+    cubes[-1].keyframe_insert(data_path='rotation_euler', frame=0)
+
     i += 1
+
+scn = bpy.context.scene
+
+for i in range(0, 50):
+    for cube in cubes:
+        cube.select = True
+
+        cube.rotation_euler[0] += math.pi / 4
+        cube.convert_space(from_space='LOCAL', to_space='WORLD')
+
+        cube.keyframe_insert(data_path='rotation_euler', frame=10 * (i + 1))
+
+        cube.select = False
+
+# Move the camera further away
+camera = bpy.data.objects['Camera']
+camera.select = True
+camera.location = (DISTANCE * 4, - DISTANCE * 4, DISTANCE * 3)
+camera.select = False
