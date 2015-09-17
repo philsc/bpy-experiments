dd5e9b10828ff24c6a0581b39a805e21fbbeda08
Philipp Schrader <philipp.schrader@gmail.com>
2015-09-16 20:14:45 -0700

    Put each elevator part on either side.

--- foo.py
+++ foo.py
@@ -35,11 +35,17 @@ class DriveBase(RobotPart):
 
   BOTTOM_PLANE_Z = 0.02619
 
+  # Two points to help calculate the horizontal center of the base.
+  HORIZONTAL_CENTER1 = (0.50648, -0.11303)
+  HORIZONTAL_CENTER2 = (-0.50648, -0.05959)
+
   def __init__(self, label):
     super().__init__(ROBOT_PARTS[3], label, axis_up='Y', axis_forward='Z')
 
   def get_reference_point(self):
     point = self.bpy_obj.location.copy()
+    point[0] = (self.HORIZONTAL_CENTER1[0] + self.HORIZONTAL_CENTER2[0]) / 2
+    point[1] = (self.HORIZONTAL_CENTER1[1] + self.HORIZONTAL_CENTER2[1]) / 2
     point[2] += self.BOTTOM_PLANE_Z
     return point
 
@@ -49,6 +55,11 @@ class Elevator(RobotPart):
   BOTTOM_PLANE_Z = 0.0
   DIST_ELEVATOR_TO_BASE = 0.33682
 
+  # TODO(phil): Figure out what this number is.
+  # This is the distance from the center of the drive base to the center of
+  # the elevator guides on the sides of the robot.
+  DIST_TO_GUIDE = 0.4
+
   def __init__(self, label, parent, mirrored=False):
     super().__init__(ROBOT_PARTS[2], label, axis_up='Y', axis_forward='Z')
     self.parent = parent
@@ -57,9 +68,10 @@ class Elevator(RobotPart):
 
   def set_height(self, height):
     self.height = height
-    self.bpy_obj.location = self.parent.get_reference_point()
-    self.bpy_obj.location[2] += (self.DIST_ELEVATOR_TO_BASE -
-        self.BOTTOM_PLANE_Z + height)
+    location = self.parent.get_reference_point()
+    location[0] += self.DIST_TO_GUIDE if self.mirrored else -self.DIST_TO_GUIDE
+    location[2] += self.DIST_ELEVATOR_TO_BASE - self.BOTTOM_PLANE_Z + height
+    self.bpy_obj.location = location
     return self
 
   def get_reference_point(self):
